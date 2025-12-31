48381b05ed6977b1ca32a36ed54af5eb422ea8b7
JeeminKim <kjeemin21@gmail.com>
2025-12-31 17:33:38 +0900

    Enhance photo gallery functionality by adding lightbox and pagination features. Implement JavaScript for lightbox interactions and pagination management. Update HTML structure for photo display and pagination controls, and improve CSS for better layout and responsiveness.

---
+++ oldpages/old_pages_alumni.php
@@ -0,0 +1,8 @@
+<ul>
+    <li class="ic_dot2"> Jaejun Lee (이재준) </li>
+    <li class="ic_dot2"> M.S. in Computing, Feb. 2023, KAIST </li>
+    <ul>
+        <li class="ic_dot4"> Thesis: Image-based Augmented Knowledge Graph Embedding </li>	
+    </ul>
+    <li class="ic_dot2"> B.S. in Computing (double major: Mathematical Sciences), Aug. 2021, KAIST </li>
+</ul>
