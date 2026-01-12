e0f1edd3e59af633689ebea57fda20d6df183fa2
JeeminKim <kjeemin21@gmail.com>
2026-01-12 10:39:33 +0900

    Removed old pages

--- oldpages/old_pages_recruit.php
+++
@@ -1,7 +0,0 @@
-<p>
-    <!-- <a href="/down/BDI_Lab_KAIST.pdf"  target="_blank" style="color:#2F32C4;"> --><a href="https://www.youtube.com/watch?v=dGNjNBvxrDA&t=1s&ab_channel=BDILab"  target="_blank"  class="txty23">연구실 동영상</a>, <a href="/down/BDI_Lab_KAIST.pdf"  target="_blank"  class="txty23">연구실 소개자료</a>, <a href="/down/BDI_Lab_video.html" target="_blank"  class="txty23">연구실 소개영상</a>, <a href="/down/BDI_Lab_internship.pdf"  target="_blank"  class="txty23">인턴 모집공고</a>, <a href="https://www.youtube.com/@bdi-lab"  target="_blank"  class="txty23">YouTube</a> <br>
-    [학생 모집 중] 석사 신입생: 
-    <span><a href="https://forms.gle/8TUm8iPkgvRQSZv89"  target="_blank" class="txty23">지원서 링크</a></span>, 
-    학부 연구생: <span><a href="https://forms.gle/vPZs2LjWPCJFCa4Z8"  target="_blank" class="txty23">지원서 링크</a></span><br>
-    우리 연구실에 관심있는 학생들은 jjwhang@kaist.ac.kr 로 문의바랍니다.
-</p>
